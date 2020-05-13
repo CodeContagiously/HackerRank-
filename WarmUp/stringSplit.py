a7de4b734f8797f6e922a47dc16659044b668ecc
PerryBoh <42015988+CodeContagiously@users.noreply.github.com>
2020-05-13 03:27:19 -0400

    Add files via upload

---
+++ WarmUp/stringSplit.py
@@ -0,0 +1,6 @@
+
+"""HackerRank question:
+https://www.hackerrank.com/challenges/python-string-split-and-join/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=7-day-campaign
+"""
+def split_and_join(line):
+    return "-".join(line.split(" "))
