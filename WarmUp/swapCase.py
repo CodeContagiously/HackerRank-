41de7cb30872c2a14ed201f8127ce163643d0e71
PerryBoh <42015988+CodeContagiously@users.noreply.github.com>
2020-05-17 00:05:08 -0400

    Add files via upload

---
+++ WarmUp/swapCase.py
@@ -0,0 +1,5 @@
+## Hackerrank problem:
+##https://www.hackerrank.com/challenges/swap-case/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+
+def swap_case(s):
+    return s.swapcase()
