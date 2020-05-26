da0a8d0a8d52c6e6e5f88f43c6c9c76a66049a36
PerryBoh <42015988+CodeContagiously@users.noreply.github.com>
2020-05-26 01:54:09 -0400

    Add files via upload

---
+++ WarmUp/Array Average.py
@@ -0,0 +1,5 @@
+#Hackerrank
+##https://www.hackerrank.com/challenges/py-introduction-to-sets/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+
+def average(array):
+    return sum(set(array))/len(set(array))
