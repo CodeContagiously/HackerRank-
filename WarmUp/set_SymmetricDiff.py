51bd6e025d7c7b558efdf487fe69cdf748c473be
PerryBoh <42015988+CodeContagiously@users.noreply.github.com>
2020-06-07 02:30:49 -0400

    Update set_SymmetricDiff.py

--- WarmUp/set_SymmetricDiff.py
+++ WarmUp/set_SymmetricDiff.py
@@ -1,10 +1,12 @@
 #Hackerrank
 ##https://www.hackerrank.com/challenges/symmetric-difference/problem?h_r=next-challenge&h_v=zen&utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign&isFullScreen=true
 
-set1 = set(input())
-set2 = set(input())
+set1Size = input() #this input was unnecessary
+set1 = set(input().split())
+set2Size = input()
+set2 = set(input().split())
 set1,set2 = set1.difference(set2), set2.difference(set1)
-for val in set1:
-    if val.isdigit(): print(val)
-for val in set2:
-    if val.isdigit(): print(val)
+finSet = [int(num) for num in list(set1) + list(set2)]
+finSet.sort()
+for val in finSet:
+    print(val)
